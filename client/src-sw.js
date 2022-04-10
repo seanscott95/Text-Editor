a533b188348a637d9d3838903f05770052bba656
Sean <seanms418@gmail.com>
2022-04-10 18:05:47 +1000

    changed the request in the register route for asset catching

--- client/src-sw.js
+++ client/src-sw.js
@@ -28,7 +28,7 @@ registerRoute(({ request }) => request.mode === 'navigate', pageCache);
 
 // TODO: Implement asset caching
 registerRoute(
-  ({ request }) => request.destination === 'static-assets',
+  ({ request }) => ['style', 'script', 'worker'].includes(request.destination),
   new CacheFirst({
     cacheName: 'my-static-assets-cache',
     plugins: [
